0d84b4f51c89bf92160b2e207ed5554a8cbe3bef
eaviles55 <ernesto.aviles@daimler.com>
2021-02-25 04:09:01 +0000

    Actualización

--- Queries.R
+++ Queries.R
@@ -1,1 +1,29 @@
 #Reto1 Sesion 7
+install.packages("DBI")
+library(DBI)
+
+MyDataBase <- dbConnect(
+  drv = RMySQL::MySQL(),
+  dbname = "shinydemo",
+  host = "shiny-demo.csa7qlmguqrf.us-east-1.rds.amazonaws.com",
+  username = "guest",
+  password = "guest")
+
+install.packages("dplyr")
+library(dplyr)
+
+dbListTables(MyDataBase)
+dbListFields(MyDataBase, 'CountryLanguage')
+
+#porcentaje de personas que hablan español en todos los países
+
+DataDB <- dbGetQuery(MyDataBase, "select * from CountryLanguage")
+summary(DataDB)
+
+espanol <- DataDB %>% filter (Language == "Spanish" ,
+                             Percentage > 0)
+#ggplot
+install.packages("ggplot2")
+library(ggplot2)
+
+ggplot(espanol, aes(x=CountryCode, y= reorder(Percentage)+geom_col()+coord_flip))
